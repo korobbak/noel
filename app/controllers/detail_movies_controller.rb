fc3e8a237f367bf5ce6ad9e96591d3f4ec14716d
korobbak <korobbak@gmail.com>
2019-03-19 19:52:54 +0700

    Add layout

--- app/controllers/detail_movies_controller.rb
+++ app/controllers/detail_movies_controller.rb
@@ -2,12 +2,14 @@ class DetailMoviesController < ApplicationController
   def show
     @movie = Movie.find_by(id: params[:id])
     @movie_type = MovieType.find_by(name: @movie.movie_types.sample.name).movie.where("status_movie=true").sort_date
-    @episode = Movie.find_by(id: params[:id]).episodes.select("id").where("status_episode=true").order("created_at DESC")
+    @episode = Movie.find_by(id: params[:id]).episodes.select("id").where("status_episode=true").order("created_at ASC")
 
     if !current_user.nil?
       @movie_like = []
       Movie.find_by(id: params[:id]).user_like_movies.map do |a|
-        @movie_like << current_user.user_like_movies.find_by(id: a.id)
+        if !current_user.user_like_movies.find_by(id: a.id).nil?
+          @movie_like << current_user.user_like_movies.find_by(id: a.id)
+        end
       end
     end
   end
--- app/controllers/watch_movies_controller.rb
+++ app/controllers/watch_movies_controller.rb
@@ -2,7 +2,7 @@ class WatchMoviesController < ApplicationController
   def show
     @movie = Episode.find_by(id: params[:id]).movie
     @episode_show = Episode.find_by(id: params[:id])
-    @episode = @movie.episodes.where("status_episode = ?", true).order("created_at DESC")
+    @episode = @movie.episodes.where("status_episode = ?", true).order("created_at ASC")
     @comments = @movie.comments
       .order("created_at DESC").page(params[:page]).per 5
     @comment = @movie.comments.build
